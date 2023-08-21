4bae3694dd0b758ccd39747cabeaee7201f15d33
Dishi <dishinayak223@gmail.com>
2023-08-21 10:34:36 +0530

    new

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,18 +1,65 @@
-import './App.css';
-import MainForm from './component/MainForm'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import ChatRoom from './component/ChatRoom';
+// routes
+// theme
+// components
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import ThemeSettings from "./components/settings";
+import ThemeProvider from "./theme";
+import Router from "./routes";
+import { closeSnackBar } from "./redux/slices/app";
+import { socket } from "./socket";
+
+const vertical = "bottom";
+const horizontal = "center";
+
+const Alert = React.forwardRef((props, ref) => (
+  <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+));
+
 function App() {
+  const dispatch = useDispatch();
+
+  const { severity, message, open } = useSelector(
+    (state) => state.app.snackbar
+  );
+
   return (
-    <div className="container-fluid bg-light text dark d-flex align-items-center justify-content-center" style={{height:"100vh"}}>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<MainForm/>}/>
-          <Route path="/chat/:roomName" element={<ChatRoom/>}/>
-          <Route path="*" element={<h1>404 not found!</h1>}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <ThemeProvider>
+        <ThemeSettings>
+          {" "}
+          <Router />{" "}
+        </ThemeSettings>
+      </ThemeProvider>
+
+      {message && open ? (
+        <Snackbar
+          anchorOrigin={{ vertical, horizontal }}
+          open={open}
+          autoHideDuration={4000}
+          key={vertical + horizontal}
+          onClose={() => {
+            console.log("This is clicked");
+            dispatch(closeSnackBar());
+          }}
+        >
+          <Alert
+            onClose={() => {
+              console.log("This is clicked");
+              dispatch(closeSnackBar());
+            }}
+            severity={severity}
+            sx={{ width: "100%" }}
+          >
+            {message}
+          </Alert>
+        </Snackbar>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
 
